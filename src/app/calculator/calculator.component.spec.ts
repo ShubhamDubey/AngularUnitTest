d0d297d494b3267c4ebdc6d67113563048cd820a
Shubham Dubey <shubhamdubey1996@gmail.com>
2020-04-20 15:55:22 +0530

    Enhanced Test cases with setup and teardown

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -3,32 +3,36 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalculatorComponent } from './calculator.component';
 
 describe('CalculatorComponent', () => {
+
+  let component: CalculatorComponent;
+
+  beforeEach(() => { // Setup
+    component = new CalculatorComponent();
+  });
+
+  afterEach(() => { // TearDown
+    component = new CalculatorComponent();
+  })
+
   it('should return addition of two numbers', () => {
-
-    let component: CalculatorComponent;
+    // let component: CalculatorComponent;
     expect(component.add(10, 10)).toEqual(20);
-
   });
 
   it('should return difference of two numbers', () => {
-
-    let component: CalculatorComponent;
+    // let component: CalculatorComponent;
     expect(component.subtract(10, 10)).toEqual(0);
   });
 
   it('should return multi of two numbers', () => {
-
-    let component: CalculatorComponent;
+    // let component: CalculatorComponent;
     expect(component.multiply(10, 10)).toEqual(100);
   });
 
   it('should return divi of two numbers', () => {
-
-    let component: CalculatorComponent;
+    // let component: CalculatorComponent;
     expect(component.divide(10, 10)).toEqual(1);
   });
-
-
 
   // let component: CalculatorComponent;
   // let fixture: ComponentFixture<CalculatorComponent>;
